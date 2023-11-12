665874b85264b2ac1f0e86225f6781677a561525
Jani Heinikoski <heinikoski.jani@gmail.com>
2023-11-12 03:53:50 +0200

    fixed timezone issues in template

--- sensor/measure.py
+++ sensor/measure.py
@@ -1,13 +1,13 @@
 import os
 import pandas as pd
 from influxdb_client_3 import InfluxDBClient3, Point, WritePrecision
-import pyarrow
 
 TOKEN = os.getenv('INFLUXDB_TOKEN')
 ORG = os.getenv('INFLUXDB_ORG')
 HOST = os.getenv('INFLUXDB_HOST')
 BUCKET = 'environment'
 MEASUREMENT = 'env_data'
+TZ = 'Europe/Helsinki'
 
 if (TOKEN == None or ORG == None or HOST == None):
     print('Please set the required environment variables to connect to the InfluxDB database.')
@@ -18,12 +18,15 @@ client = InfluxDBClient3(token=TOKEN,
                          org=ORG,
                          database=BUCKET)
 
-ex_ts = pd.Timestamp.now(tz="Europe/Helsinki") - pd.Timedelta(days=29)
+# Example timestamp from 29 days ago (Helsinki timezone)
+ex_ts = pd.Timestamp.now(tz=TZ) - pd.Timedelta(days=29)
 point = Point(MEASUREMENT).tag("sensor", "linux-ubuntu-vm").field("temperature", float(23.5)).time(time=ex_ts, write_precision=WritePrecision.S)
 
 client.write(point)
 
 query = 'select * from %s' % (MEASUREMENT)
 df: pd.DataFrame = client.query(query=query, language="sql").to_pandas()
-print(df.head(), end='\n\n\n')
+# Localize the times back to Helsinki timezone
+df['time'] = df['time'].dt.tz_localize(TZ)
+print(df.head(100), end='\n\n\n')
 print(df.info())
