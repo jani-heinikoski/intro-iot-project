d5a95f49e76c736cdbd4758a417eae16690e0eab
Jani Heinikoski <heinikoski.jani@gmail.com>
2023-11-12 15:33:51 +0200

    generate measurements script + deps ver fix

--- sensor/measure.py
+++ sensor/measure.py
@@ -1,32 +1,95 @@
+# Authors: Jani Heinikoski, Vili Huusko
+# Sources:
+# - https://github.com/InfluxCommunity/influxdb3-python
+# - https://pandas.pydata.org/docs/
+# - https://pythonhosted.org/sense-hat/api/
+
 import os
 import pandas as pd
+import random
+from typing import List, Dict
 from influxdb_client_3 import InfluxDBClient3, Point, WritePrecision
+from sense_emu import SenseHat
 
+# Environment variables for connecting to the InfluxDB database
 TOKEN = os.getenv('INFLUXDB_TOKEN')
 ORG = os.getenv('INFLUXDB_ORG')
 HOST = os.getenv('INFLUXDB_HOST')
+# Global variables, can change as per needed
 BUCKET = 'environment'
 MEASUREMENT = 'env_data'
+SENSOR_TAG = 'linux-ubuntu-vm'
 TZ = 'Europe/Helsinki'
 
-if (TOKEN == None or ORG == None or HOST == None):
-    print('Please set the required environment variables to connect to the InfluxDB database.')
-    exit(-1)
 
-client = InfluxDBClient3(token=TOKEN,
-                         host=HOST,
-                         org=ORG,
-                         database=BUCKET)
+def read_measurement(sense: SenseHat) -> Dict[str, float]:
+    return {
+        "temperature": float(sense.get_temperature()),
+        "pressure": float(sense.get_pressure()),
+        "humidity": float(sense.get_humidity())
+    }
 
-# Example timestamp from 29 days ago (Helsinki timezone)
-ex_ts = pd.Timestamp.now(tz=TZ) - pd.Timedelta(days=29)
-point = Point(MEASUREMENT).tag("sensor", "linux-ubuntu-vm").field("temperature", float(23.5)).time(time=ex_ts, write_precision=WritePrecision.S)
 
-client.write(point)
+def introduce_randomness(measurement: Dict[str, float], tmp_rng_boundary: float) -> None:
+    r = lambda b : random.uniform(-b, b) * random.random()
+    measurement["temperature"] = measurement["temperature"] + random.uniform(-tmp_rng_boundary, -tmp_rng_boundary + 0.6)
+    measurement["pressure"] = measurement["pressure"] + r(20)
+    measurement["humidity"] = measurement["humidity"] + r(2)
 
-query = 'select * from %s' % (MEASUREMENT)
-df: pd.DataFrame = client.query(query=query, language="sql").to_pandas()
-# Localize the times back to Helsinki timezone
-df['time'] = df['time'].dt.tz_localize(TZ)
-print(df.head(100), end='\n\n\n')
-print(df.info())
+
+def generate_example_data(start: pd.Timestamp, end: pd.Timestamp, freq: str, sense: SenseHat) -> List[Point]:
+    points: List[Point] = []
+    dt_range = pd.date_range(start=start, end=end, freq=freq, tz=TZ)
+    tmp_rng_boundary = 0
+
+    for dt in dt_range:
+        # read a measurement from the Sense HAT emulator
+        m = read_measurement(sense)
+        tmp_rng_boundary = tmp_rng_boundary + 5 / dt_range.size
+
+        # make night time temperatures colder than daytime
+        if (dt.time().hour >= 18 and dt.time().hour <= 23):
+            introduce_randomness(m, tmp_rng_boundary + (dt.time().hour / 23) / 2)
+        elif (dt.time().hour >= 0 and dt.time().hour <= 6):
+            introduce_randomness(m, tmp_rng_boundary + ((23 - dt.time().hour) / 23) / 2)
+        else:
+            introduce_randomness(m, tmp_rng_boundary)
+        
+        # create the data point based on the measurement from the Sense HAT emulator
+        point = Point(MEASUREMENT).tag("sensor", SENSOR_TAG) \
+        .field("temperature", m["temperature"]) \
+        .field("pressure", m["pressure"]) \
+        .field("humidity", m["humidity"]) \
+        .time(time=dt, write_precision=WritePrecision.S)
+        # add the data point to the list
+        points.append(point)
+        
+    return points
+
+
+def are_env_variables_set() -> bool:
+    return not (TOKEN == None or ORG == None or HOST == None)
+
+
+def main() -> None:
+    # Check the environment variables required for creating the InfluxDB client object
+    if (not are_env_variables_set()):
+        print('Please set the required environment variables to connect to the InfluxDB database.')
+        exit(-1)
+
+    # Generate the example data
+    start_dt = pd.Timestamp.now(tz=TZ) - pd.Timedelta(days=7)
+    end_dt = pd.Timestamp.now(tz=TZ)
+    example_data = generate_example_data(start=start_dt, end=end_dt, freq="1H", sense=SenseHat())
+
+    # Open a connection to the database
+    client = InfluxDBClient3(token=TOKEN, host=HOST, org=ORG, database=BUCKET)
+
+    client.write(example_data)
+
+    # Close the connection to the database
+    client.close()
+    
+
+if (__name__ == '__main__'):
+    main()
